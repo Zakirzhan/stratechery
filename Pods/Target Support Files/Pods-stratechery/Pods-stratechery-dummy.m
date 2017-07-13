95a1d9afe54b852e93bbe689b764493ea2bb7024
Aisabaev Zakirzhan <zako1996@mail.ru>
2017-07-13 15:00:32 +0600

    remove pods

--- Pods/Target Support Files/Pods-stratechery/Pods-stratechery-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_stratechery : NSObject
-@end
-@implementation PodsDummy_Pods_stratechery
-@end
--- Pods/Target Support Files/Pods-stratecheryTests/Pods-stratecheryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_stratecheryTests : NSObject
-@end
-@implementation PodsDummy_Pods_stratecheryTests
-@end
